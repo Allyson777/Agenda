4282573f923f2c440340f4340f5aef96a46169a5
Allyson Pina <allyson.viana@edu.ufes.br>
2024-08-27 19:56:55 -0300

    :sparkles: :feat componentForms

--- frontend/src/components/InputtForms.tsx
+++ frontend/src/components/InputtForms.tsx
@@ -2,12 +2,14 @@
 interface InputProps {
     type: string;
     placeholder: string; // Definindo placeholder como uma propriedade
+    value: string;
+    onChange: (e:React.ChangeEvent<HTMLInputElement>) => void;
   }
-export default function InputForms({placeholder, type}: InputProps){
+export default function InputForms({placeholder, type, value, onChange}: InputProps){
     return(
     <div className='w-full'> 
         <input className="w-full h-10 bg-slate-600 shadow-md rounded-md hover:scale-110 hover:bg-sky-950 !pl-2" 
-        type={type} placeholder={placeholder}/>
+        type={type} placeholder={placeholder} value={value} onChange={onChange}/>
     </div>
     )
 }
